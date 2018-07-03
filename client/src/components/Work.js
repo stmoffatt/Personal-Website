0ea970c638688975aee7c70d7f8a027b9f45b06d
Sean Moffatt <seanmoffatt9@gmail.com>
2018-07-02 19:28:35 -0700

    a href change

--- client/src/components/Work.js
+++ client/src/components/Work.js
@@ -4,7 +4,6 @@ import SpotiFoundImage from './images/spotifound.png'
 import LiteratoImage from './images/literato.png'
 import BattleShip from './images/battleship.png'
 import Personal from './images/personal.png'
-import { Link } from 'react-router-dom'
 
 class Work extends Component {
   render() {
@@ -12,31 +11,31 @@ class Work extends Component {
       <div>
         <div className="image-box" id="imageLeft">
           <img src={SpotiFoundImage} alt="SpotiFound Website" className="image" />
-          <Link to="/Spoti-Found" className="overlay">
+          <a href="/Spoti-Found" className="overlay">
             <div className="header">Spoti-Found</div>
             <div className="subtitle">React - Frontend - API </div>
-          </Link>
+          </a>
         </div>
         <div className="image-box" id="imageRight">
           <img src={LiteratoImage} alt="Literato Website" className="image" />
-          <Link to="/literato" className="overlay">
+          <a href="/literato" className="overlay">
             <div className="header">Literato </div>
             <div className="subtitle">React - Frontend - Backend - API </div>
-          </Link>
+          </a>
         </div>
         <div className="image-box" id="imageLeft">
           <img src={BattleShip} alt="BattleShip Website" className="image" />
-          <Link to="/battleship" div className="overlay">
+          <a href="/battleship" div className="overlay">
             <div className="header">BattleShip</div>
             <div className="subtitle">React - Frontend </div>
-          </Link>
+          </a>
         </div>
         <div className="image-box" id="imageRight">
           <img src={Personal} alt="Personal Website" className="image" />
-          <Link to="/literato" className="overlay">
+          <a href="/literato" className="overlay">
             <div className="header">Personal WebPage</div>
             <div className="subtitle">React - Frontend - Backend</div>
-          </Link>
+          </a>
         </div>
       </div>
     )
